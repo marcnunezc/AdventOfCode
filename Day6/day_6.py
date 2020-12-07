60c6baf8d4eb9da31bc7d20a636be67c5ee50461
marcnunezc <mnunez@cimne.upc.edu>
2020-12-07 11:09:20 +0100

    Refactor day 6 2

--- Day6/day_6.py
+++ Day6/day_6.py
@@ -1,4 +1,4 @@
-lines_list = open("input.txt").read().split("\n\n")
+lines_list = open("input.txt").read()[:-1].split("\n\n")
 
 counts = sum([len({char for char in questionary if char != "\n"}) for questionary in lines_list])
 print("Part 1 number of counts 6565", counts)
@@ -7,8 +7,7 @@ counts = 0
 for questionary in lines_list:
     aux_set = {char for char in questionary.split("\n")[0]}
     for line in questionary.split("\n")[1:]:
-        if line != "":
-            aux_set=aux_set.intersection({char for char in line})
+        aux_set=aux_set.intersection({char for char in line if char != ""})
     counts += len(aux_set)
     is_first_line = True
 print("Part 2 number of counts 3137", counts)
