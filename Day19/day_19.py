4f4d337c7779f61c985ee8c7d8d7de2405427d67
marcnunezc <mnunez@cimne.upc.edu>
2020-12-19 20:07:57 +0100

    developing day 19

--- Day19/day_19.py
+++ Day19/day_19.py
@@ -56,30 +56,41 @@ dict_history = {}
 def check_rule2(line, rule_key):
     # if not sum(get_length(rule) for rule in rules[rule_key][0]) == len(line):
         # return False
-    if len(line)==0:
+    if line=="":
         return False
+    stopnow = False
+    for rule in rules[rule_key]:
+        if rule_key in rule:
+            stopnow=True
+            copy_to_replace = copy.deepcopy(rules[rule_key])
+            rules[rule_key].pop()
+            for j in range(1, 2):
+                aux_list = []
+                for x in copy_to_replace[-1]:
+                    if x == rule_key:
+                        for y in copy_to_replace[1]:
+                            aux_list.append(y)
+                    else:
+                        aux_list.append(x)
+                copy_to_replace.append(aux_list)
+                rules[rule_key].append([x for x in aux_list if x != rule_key])
+
     rule_check = False
     starting_length = 0
     for rule in rules[rule_key]:
-        print("################## Rule", rule)
+        # print("################## Rule", rule)
         starting_sub_length = starting_length
         sub_rule_check = True
         for subrule in rule:
-            print("######### Subrule", subrule)
             this_length = starting_sub_length + get_length(subrule)
-            # rule_set
-            # if not
             this_check = check_sub_rule2(line[starting_sub_length:this_length], subrule)
-
+            print(line[starting_sub_length:this_length])
             sub_rule_check = min(this_check, sub_rule_check)
-            print("Check", line[starting_sub_length:this_length], subrule, this_check, sub_rule_check)
             starting_sub_length = this_length
         rule_check = max(sub_rule_check, rule_check)
     return rule_check
 
 rules["8"] = [['42'], ['42','8']]
-# rules["11"] =  [['42','31'], ['42','11','31']]
-print(rules["0"])
-print(rules["0"])
+rules["11"] =  [['42','31'], ['42','11','31']]
 part_2=len([check for check in (check_rule2(line,"0") for line in lines_list[i:]) if check])
 print("Part 2:",  part_2)
