c3bf6b960ed72c2c6a093674f4202adb4c56c14b
mnunezc <mnunez@cimne.upc.edu>
2021-12-21 23:13:52 +0100

    Add day 21

---
+++ 2021/Day21/day_21.py
@@ -0,0 +1,72 @@
+import itertools
+from functools import cache
+class Dice:
+    def __init__(self):
+        self.value = 0
+        self.rolling_times = 0
+    def roll(self):
+        self.rolling_times += 1
+        self.value += 1
+        if self.value > 100:
+            self.value = 1
+        return self.value
+    def get_rolling_times(self):
+        return self.rolling_times
+
+lines_list = open('test.txt').read().splitlines()
+player_positions = [int(line.split(":")[1].strip()) for line in lines_list]
+player_scores = [0] * len(player_positions)
+dice = Dice()
+while not any([score>=1000 for score in player_scores]):
+    for i, position in enumerate(player_positions):
+        new_score = [dice.roll() for _ in range(3)]
+        player_positions[i] += sum(new_score)
+        while player_positions[i]>10:
+            player_positions[i] -= 10
+        player_scores[i] += player_positions[i]
+        if player_scores[i]>=1000:
+            break
+print("Part 1 ", dice.get_rolling_times()*min(player_scores))
+
+
+player_positions = [int(line.split(":")[1].strip()) for line in lines_list]
+player_scores = [0] * len(player_positions)
+dice_outcomes = []
+for i in range(1, 4):
+    for j in range(1, 4):
+        for k in range(1, 4):
+            dice_outcomes.append(i+j+k)
+def sum_outcome(pos, outcome):
+    pos += outcome
+    while pos>10:
+        pos -= 10
+    return pos
+
+
+# @cache
+def play_realization(pos_i, pos_j, score_i, score_j, wins_i, wins_j,  product):
+    if score_i  >= 21:
+        wins_i += product
+        return wins_i, wins_j
+
+    if score_j >= 21:
+        wins_j += product
+        return wins_i, wins_j
+
+    for outcome_i, outcome_j in itertools.product(outcomes_wins.keys(), repeat=2):
+        new_pos_i = sum_outcome(pos_i, outcome_i)
+        new_pos_j = sum_outcome(pos_j, outcome_j)
+        current_product = product*outcomes_wins[outcome_i]*outcomes_wins[outcome_j]
+        wins_i, wins_j= play_realization(new_pos_i, new_pos_j, score_i+new_pos_i, score_j+new_pos_j, wins_i, wins_j, current_product)
+    return wins_i, wins_j
+
+import time
+start_time = time.time()
+outcomes_wins = {value:dice_outcomes.count(value) for value in dice_outcomes}
+wins_i = 0
+wins_j = 0
+wins_i, wins_j=play_realization(player_positions[0], player_positions[1], player_scores[0], player_scores[1], 0, 0,1)
+
+print("Part 2 ", max(wins_i//len(dice_outcomes),wins_j))
+print("--- %s seconds ---" % (time.time() - start_time))
+
---
+++ 2021/Day21/random.py
@@ -0,0 +1,80 @@
+from collections import Counter, namedtuple, defaultdict
+from itertools import product, cycle
+from typing import List
+
+# from aoc import get_lines, take
+
+
+def parse_input(lines: List[str]) -> List[int]:
+    return [int(line.split(' ')[-1]) for line in lines if len(line) > 0]
+
+
+# def part_1(pos: List[int], score_to_win: int = 1000) -> int:
+#     scores = [0, 0]
+#     dice_total = 0
+#     dice_cycle = cycle(range(1, 101))
+#     while True:
+#         for i in range(len(pos)):
+#             s = sum(take(3, dice_cycle))
+#             dice_total += 3
+#             pos[i] = wrap(pos[i] + s, 10)
+#             scores[i] += pos[i]
+#             if scores[i] >= score_to_win:
+#                 return dice_total * scores[0 if i == 1 else 1]
+
+
+def part_2(pos: List[int], score_to_win: int = 21) -> int:
+    # outcomes = [sum([a, b, c]) for c in range(1, 4) for b in range(1, 4) for a in range(1, 4)]
+    outcomes= [1,1,1,2,2,3]
+    print(outcomes)
+    possible_sums = Counter(outcomes)
+    pos_scores = defaultdict(int)
+    pos_scores[(PScores(pos[0], 0), PScores(pos[1], 0))] = 1
+    possible_sums_two = {(p[0], p[1]): possible_sums[p[0]] * possible_sums[p[1]]
+                         for p in product(possible_sums.keys(), possible_sums.keys())}
+    player_1_wins = 0
+    player_2_wins = 0
+    while len(pos_scores) > 0:
+        new_pos_scores = defaultdict(int)
+        for ps, n_times in pos_scores.items():
+            already_won = set()
+            for k, v in possible_sums_two.items():
+                player1, player2 = ps
+                new_pos_1 = wrap(player1.pos + k[0], 10)
+                new_score_1 = player1.score + new_pos_1
+                new_pos_2 = wrap(player2.pos + k[1], 10)
+                new_score_2 = player2.score + new_pos_2
+                n_universes = n_times * v
+                if new_score_1 >= score_to_win:
+                    if k[0] not in already_won:
+                        player_1_wins += (n_times * possible_sums[k[0]])
+                    already_won.add(k[0])
+                elif new_score_2 >= score_to_win:
+                    player_2_wins += n_universes
+                else:
+                    new_pos_scores[(PScores(new_pos_1, new_score_1), PScores(new_pos_2, new_score_2))] += n_universes
+        pos_scores = new_pos_scores
+    return max(player_1_wins, player_2_wins)
+
+
+def wrap(val: int, base: int) -> int:
+    quotient, remainder = divmod(val, base)
+    if remainder > 0:
+        return remainder
+    return base
+
+
+class PScores(namedtuple('PScores', 'pos, score')):
+    def __repr__(self):
+        return f"{self.pos} {self.score}"
+
+
+def main():
+    lines = open("test.txt").read().splitlines()
+    pos = parse_input(lines)
+    # print("Part 1:", part_1(pos.copy()))
+    print("Part 2:", part_2(pos))
+
+
+if __name__ == '__main__':
+    main()
