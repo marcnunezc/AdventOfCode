c7638afef92a5fec23c8041f41ffcdd72e7007a9
mnunezc <mnunez@cimne.upc.edu>
2021-12-22 01:01:52 +0100

    Use cache

--- 2021/Day21/day_21.py
+++ 2021/Day21/day_21.py
@@ -15,21 +15,22 @@ class Dice:
     def get_rolling_times(self):
         return self.rolling_times
 
-# @cache
-def play_realization(pos_i, pos_j, score_i, score_j, wins_i, wins_j,  product):
+@cache
+def play_realization(pos_i, pos_j, score_i, score_j):
     if score_i  >= 21:
-        wins_i += product
-        return wins_i, wins_j
-
+        return 1, 0
     if score_j >= 21:
-        wins_j += product
-        return wins_i, wins_j
-
-    for outcome_i, outcome_j in itertools.product(outcomes_wins.keys(), repeat=2):
+        return 0, 1
+    wins_i, wins_j = 0,0
+    for outcome_i, outcome_j in itertools.product(outcomes_counts.keys(), repeat=2):
         new_pos_i = sum_outcome(pos_i, outcome_i)
         new_pos_j = sum_outcome(pos_j, outcome_j)
-        current_product = product*outcomes_wins[outcome_i]*outcomes_wins[outcome_j]
-        wins_i, wins_j= play_realization(new_pos_i, new_pos_j, score_i+new_pos_i, score_j+new_pos_j, wins_i, wins_j, current_product)
+        new_score_i = score_i + new_pos_i
+        new_score_j = score_j + new_pos_j
+        new_wins_i, new_wins_j = play_realization(new_pos_i, new_pos_j, new_score_i, new_score_j)
+        product = outcomes_counts[outcome_i]*outcomes_counts[outcome_j]
+        wins_i += new_wins_i*product
+        wins_j += new_wins_j*product
     return wins_i, wins_j
 
 def sum_outcome(pos, outcome):
@@ -58,13 +59,9 @@ print("Part 1 ", dice.get_rolling_times()*min(player_scores))
 
 player_positions = [int(line.split(":")[1].strip()) for line in lines_list]
 player_scores = [0] * len(player_positions)
-dice_outcomes = []
-for i in range(1, 4):
-    for j in range(1, 4):
-        for k in range(1, 4):
-            dice_outcomes.append(i+j+k)
-outcomes_wins = {value:dice_outcomes.count(value) for value in dice_outcomes}
-wins_i, wins_j=play_realization(player_positions[0], player_positions[1], player_scores[0], player_scores[1], 0, 0,1)
+dice_outcomes = [i+k+j for i in range(1,4) for j in range(1,4) for k in range(1,4)]
+outcomes_counts = {value:dice_outcomes.count(value) for value in dice_outcomes}
+wins_i, wins_j=play_realization(player_positions[0], player_positions[1], player_scores[0], player_scores[1])
 
 print("Part 2 ", max(wins_i//len(dice_outcomes),wins_j))
 print("--- %s seconds ---" % (time.time() - start_time))
