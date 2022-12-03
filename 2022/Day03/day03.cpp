2d13520ef65546b4bb4204a10700d40fcf68e424
marcnunezc <marcnc94@gmail.com>
2022-12-03 11:53:12 +0100

    Remove not needed vars

--- 2022/Day03/day03.cpp
+++ 2022/Day03/day03.cpp
@@ -17,8 +17,7 @@ AOC_DAY(Day03_1){
     while(getline(cin, line)) {
         auto first_half = line.substr(0, line.length()/2);
         for (char c : first_half) {
-            auto found = line.find(c, line.length()/2);
-            if (found != string::npos) {
+            if (line.find(c, line.length()/2) != string::npos) {
                 total += score[c];
                 break;
             }
@@ -40,9 +39,7 @@ AOC_DAY(Day03_2){
         else {
             std::string first = group_rucksack[0];
             for (char c : first) {
-                char found1 = group_rucksack[1].find(c);
-                char found2 = group_rucksack[2].find(c);
-                if (found1 != string::npos && found2 != string::npos) {
+                if (group_rucksack[1].find(c) != string::npos && group_rucksack[2].find(c) != string::npos) {
                     total += score[c];
                     break;
                 }
