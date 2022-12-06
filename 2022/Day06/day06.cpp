af44154440ef3fe7779af5ac02aed6a1f18548cd
marcnunezc <marcnc94@gmail.com>
2022-12-06 09:01:19 +0100

    Use unordered set

--- 2022/Day06/day06.cpp
+++ 2022/Day06/day06.cpp
@@ -2,14 +2,15 @@ void compute_day_06_solution(size_t packet_length) {
     std::string line;
     getline(cin, line);
     size_t i;
+    std::unordered_set<char> packet_marker;
     for (i = 0; i<line.length(); i++) {
-        std::map<char, size_t> packet_marker;
         for (size_t j=0; j<packet_length; j++) {
-            packet_marker[line[i+j]]=i+j;
+            packet_marker.insert(line[i+j]);
         }
         if (packet_marker.size() == packet_length) {
             break;
         }
+        packet_marker.clear();
     }
     cout << i+packet_length << endl;
 }
--- 2022/include/main.h
+++ 2022/include/main.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <numeric>
 #include <set>
+#include <unordered_set>
 #include <cstring>
 
 using namespace std;
