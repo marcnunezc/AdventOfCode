1b100e1373573d54cea002d1c290790592dd6177
marcnunezc <marcnc94@gmail.com>
2022-12-06 09:55:50 +0100

    Add option -l to list all days and -a to run all days

--- 2022/include/main.h
+++ 2022/include/main.h
@@ -7,6 +7,7 @@
 #include <set>
 #include <unordered_set>
 #include <cstring>
+#include <fstream>
 
 using namespace std;
 
--- 2022/src/main.cpp
+++ 2022/src/main.cpp
@@ -22,10 +22,33 @@ int main(int argc, char *argv[]) {
     if (argc != 2) {
         cout << "Please provide day and part in the format: Day##_#" << endl;
         cout << "e.g: aoc22 Day01_1 < Day01/input.txt" << endl;
+        cout << "Use -l to list all days" << endl;
+        cout << "Use -a to run all days" << endl;
         exit(1);
     }
 
     std::string name(argv[1]);
+    if (!strcmp(argv[1], "-l")) {
+        for (auto it = DayFactory::Days.begin(); it != DayFactory::Days.end(); it++) {
+            cout << it->first << endl;
+        }
+        return 0;
+    }
+    if (!strcmp(argv[1], "-a")) {
+        for (auto it = DayFactory::Days.begin(); it != DayFactory::Days.end(); it++) {
+            cout << "Running " << it->first << " ";
+            std::string basename = it->first.substr(0, 5);
+            std::string input_file(basename+"/input.txt");
+            std::ifstream in(input_file);
+            std::cin.rdbuf(in.rdbuf()); //redirect std::cin to in.txt!
+            auto start = std::chrono::high_resolution_clock::now();
+            cout << it->second() << " ";
+            auto stop = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+            cout << "Time: " << duration.count() << " ms"<<endl;
+        }
+        return 0;
+    }
     if (DayFactory::Days.find(name) == DayFactory::Days.end()) {
         cout << "Day with name: " << name << " not found" << endl;
         exit(1);
