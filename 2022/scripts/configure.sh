1106ac4ffe8af583880cb4f98b14c4bf1b2d020c
marcnunezc <marcnc94@gmail.com>
2022-12-08 18:26:43 +0100

    Oops

--- 2022/scripts/configure.sh
+++ 2022/scripts/configure.sh
@@ -3,7 +3,7 @@ export CXX=g++-10
 export SOURCE_PATH="${SOURCE_PATH:-"$( cd "$(dirname "$0")" ; pwd -P )"/..}"
 export BUILD_PATH="${SOURCE_PATH}/build"
 
-cmake -H"${SOURCE_PATH}" -B"${BUILD_PATH}/${KRATOS_BUILD_TYPE}" \
--DCMAKE_BUILD_TYPE=Debug
+cmake -H"${SOURCE_PATH}" -B"${BUILD_PATH}" \
+-DCMAKE_BUILD_TYPE=Release
 
 cmake --build "${BUILD_PATH}"
