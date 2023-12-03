33ee2951a9536900999f9ec3618e17aa1393dea0
Marc Nuñez <marcnc94@gmail.com>
2023-12-03 16:48:35 +0100

    Some clean up day03

--- 2023/Day03/day03.cpp
+++ 2023/Day03/day03.cpp
@@ -4,8 +4,8 @@ struct pair_hash {
     }
 };
 
-bool is_symbol_a_neighbour(std::vector<std::pair<int,int>>& number_positions, std::unordered_map<std::pair<int,int>, string, pair_hash>& symbol_positions) {
-    for (auto& pos : number_positions) {
+bool any_neighbour_is_symbol(std::vector<std::pair<int,int>>& digit_positions, std::unordered_map<std::pair<int,int>, string, pair_hash>& symbol_positions) {
+    for (auto& pos : digit_positions) {
         auto i = pos.first;
         auto j = pos.second;
         for (int x = i - 1; x <= i + 1; ++x) {
@@ -20,14 +20,14 @@ bool is_symbol_a_neighbour(std::vector<std::pair<int,int>>& number_positions, st
     return false;
 }
 
-void register_neighbours_to_symbol(std::vector<std::pair<int,int>>& number_positions, std::unordered_map<std::pair<int,int>, std::vector<int>, pair_hash>& symbol_positions, int number) {
-    for (auto& pos : number_positions) {
+void register_neighbours_to_symbol(std::vector<std::pair<int,int>>& digit_positions, std::unordered_map<std::pair<int,int>, std::vector<int>, pair_hash>& symbol_position_to_neighbour_numbers, int number) {
+    for (auto& pos : digit_positions) {
         auto i = pos.first;
         auto j = pos.second;
         for (int x = i - 1; x <= i + 1; ++x) {
             for (int y = j - 1; y <= j + 1; ++y) {
-                if (symbol_positions.find({x,y}) != symbol_positions.end()) {
-                    symbol_positions[{x,y}].push_back(number);
+                if (symbol_position_to_neighbour_numbers.find({x,y}) != symbol_position_to_neighbour_numbers.end()) {
+                    symbol_position_to_neighbour_numbers[{x,y}].push_back(number);
                     return;
                 }
             }
@@ -40,45 +40,37 @@ void register_neighbours_to_symbol(std::vector<std::pair<int,int>>& number_posit
 AOC_DAY(Day03_1){
     size_t sum = 0;
     std::string line;
-    std::vector<std::vector<int>> engine;
     std::vector<int> number_list;
-    std::vector<std::vector<std::pair<int,int>>> number_position_list;
+    std::vector<std::vector<std::pair<int,int>>> number_digit_positions_list;
     std::unordered_map<std::pair<int,int>, string, pair_hash> symbol_positions;
     int i=0;
     while (getline(cin, line)) {
         std::string str_number;
-        std::vector<std::pair<int,int>> positions;
+        std::vector<std::pair<int,int>> digit_positions;
         for (int j=0; j<line.length(); ++j) {
             if (isdigit(line[j])) {
                 str_number += line[j];
-                positions.push_back({i, j});
+                digit_positions.push_back({i, j});
             }
             else {
                 if (line[j] != '.') {
                     symbol_positions[{i,j}]=line[j];
                 }
-                if (str_number.length()>0) {
-                    number_list.push_back(stoi(str_number));
-                    number_position_list.push_back(positions);
-                    str_number.clear();
-                    positions.clear();
-                }
+            }
+            if ((!isdigit(line[j]) || j==line.length()-1) && str_number.length()>0) {
+                number_list.push_back(stoi(str_number));
+                number_digit_positions_list.push_back(digit_positions);
+                str_number.clear();
+                digit_positions.clear();
             }
         }
-        if (str_number.length()>0) {
-            number_list.push_back(stoi(str_number));
-            number_position_list.push_back(positions);
-            str_number.clear();
-            positions.clear();
-        }
-
         i++;
     }
     for (int i=0; i<number_list.size(); i++) {
         auto& number = number_list[i];
-        auto& positions = number_position_list[i];
+        auto& digit_positions = number_digit_positions_list[i];
 
-        if (is_symbol_a_neighbour(positions, symbol_positions)) {
+        if (any_neighbour_is_symbol(digit_positions, symbol_positions)) {
             sum += number;
         }
 
@@ -89,53 +81,45 @@ AOC_DAY(Day03_1){
 AOC_DAY(Day03_2){
     size_t sum = 0;
     std::string line;
-    std::vector<std::vector<int>> engine;
     std::vector<int> number_list;
-    std::vector<std::vector<std::pair<int,int>>> number_position_list;
-    std::unordered_map<std::pair<int,int>, std::vector<int>, pair_hash> symbol_positions;
+    std::vector<std::vector<std::pair<int,int>>> number_digit_positions_list;
+    std::unordered_map<std::pair<int,int>, std::vector<int>, pair_hash> symbol_position_to_neighbour_numbers;
     int i=0;
     while (getline(cin, line)) {
         std::string str_number;
-        std::vector<std::pair<int,int>> positions;
+        std::vector<std::pair<int,int>> digit_positions;
         for (int j=0; j<line.length(); ++j) {
             if (isdigit(line[j])) {
                 str_number += line[j];
-                positions.push_back({i, j});
+                digit_positions.push_back({i, j});
             }
             else {
                 if (line[j] == '*') {
-                    symbol_positions[{i,j}]=std::vector<int>();
-                }
-                if (str_number.length()>0) {
-                    number_list.push_back(stoi(str_number));
-                    number_position_list.push_back(positions);
-                    str_number.clear();
-                    positions.clear();
+                    symbol_position_to_neighbour_numbers[{i,j}] = {};
                 }
             }
-        }
-        if (str_number.length()>0) {
-            number_list.push_back(stoi(str_number));
-            number_position_list.push_back(positions);
-            str_number.clear();
-            positions.clear();
+            if ((!isdigit(line[j]) || j==line.length()-1) && str_number.length()>0) {
+                number_list.push_back(stoi(str_number));
+                number_digit_positions_list.push_back(digit_positions);
+                str_number.clear();
+                digit_positions.clear();
+            }
         }
         i++;
     }
 
     for (int i=0; i<number_list.size(); i++) {
         auto& number = number_list[i];
-        auto& positions = number_position_list[i];
+        auto& digit_positions = number_digit_positions_list[i];
 
-        register_neighbours_to_symbol(positions, symbol_positions, number);
+        register_neighbours_to_symbol(digit_positions, symbol_position_to_neighbour_numbers, number);
     }
 
-    for (auto& values : symbol_positions) {
+    for (auto& values : symbol_position_to_neighbour_numbers) {
         if (values.second.size()==2) {
             sum += values.second[0]*values.second[1];
         }
     }
-
 
     return std::to_string(sum);
 }
