385946dd0cd9a46e0445edcfc1a004415c78312d
marcnunezc <marcnc94@gmail.com>
2024-12-01 20:23:18 +0100

    Minor update day01

--- 2024/Day01/day01.cpp
+++ 2024/Day01/day01.cpp
@@ -10,11 +10,11 @@ AOC_DAY(Day01_1){
         list2.push_back(num2);
     }
 
-    std::sort(list1.begin(), list1.end());
-    std::sort(list2.begin(), list2.end());
+    std::ranges::sort(list1);
+    std::ranges::sort(list2);
 
-    unsigned int sum=0;
-    for (unsigned int i = 0; i<list1.size(); i++)
+    std::size_t sum=0;
+    for (std::size_t i = 0; i<list1.size(); i++)
     {
         sum += std::abs(list1[i]-list2[i]);
     }
@@ -35,7 +35,7 @@ AOC_DAY(Day01_2){
     }
 
     std::unordered_map<int, int> number_apperance;
-    unsigned int sum=0;
+    std::size_t sum=0;
 
     for (auto num : list2) {
         number_apperance[num]++;
