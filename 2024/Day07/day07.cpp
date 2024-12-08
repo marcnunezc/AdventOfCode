37b11ddbce289250dcce81589220f5502bdf4e9e
Marc Nuñez <marcnc94@gmail.com>
2024-12-08 21:26:15 +0100

    Faster day07 with dfs isntead of bit permutations

--- 2024/Day07/day07.cpp
+++ 2024/Day07/day07.cpp
@@ -1,3 +1,51 @@
+size_t compute_number_of_digits(long int number) {
+    size_t digits = 1;
+    while(number/10) {
+        digits++;
+        number = number/10;
+    }
+    return digits;
+}
+
+bool compute_operation(std::vector<int>& numbers, long int target, long int result, size_t number_of_operators, int operation, int i_operation, bool part_2 = false) {
+
+    if (i_operation>number_of_operators)
+        return target == result;
+
+    if (result > target)
+        return false;
+
+    if (i_operation == 0) {
+        if (operation==0) // sum
+            result = numbers[i_operation] + numbers[i_operation+1];
+        else if (operation==1)          //product
+            result = numbers[i_operation]*numbers[i_operation+1];
+        else
+            result = numbers[i_operation]*std::pow(10,compute_number_of_digits(numbers[i_operation+1])) + numbers[i_operation+1];
+
+        i_operation+=2;
+    } else {
+        if (operation==0) // sum
+            result += numbers[i_operation];
+        else if (operation==1)          //product
+            result *= numbers[i_operation];
+        else // concatenate
+            result = result*std::pow(10,compute_number_of_digits(numbers[i_operation])) + numbers[i_operation];;
+        i_operation++;
+    }
+
+    if (compute_operation(numbers, target, result, number_of_operators, 0, i_operation, part_2))
+        return true;
+    if (compute_operation(numbers, target, result, number_of_operators, 1, i_operation, part_2))
+        return true;
+    if (part_2 && compute_operation(numbers, target, result, number_of_operators, 2, i_operation, part_2))
+        return true;
+
+    return false;
+
+}
+
+
 AOC_DAY(Day07_1) {
 
     std::string line;
@@ -15,42 +63,18 @@ AOC_DAY(Day07_1) {
         long int result;
         auto number_of_operators = numbers.size()-1;
         size_t number_of_permutations = 1 << number_of_operators;
-        for (size_t permutation = 0; permutation<number_of_permutations; permutation++) {
-            size_t i=0;
-            for (int bit = number_of_operators-1; bit >= 0; --bit) {
-                bool operation = ((permutation >> bit) & 1);
-                if (bit == number_of_operators-1) //first computation
-                {
-                    if (operation) // sum
-                        result = numbers[i] + numbers[i+1];
-                    else          //product
-                        result = numbers[i]*numbers[i+1];
-                    i+=2;
-                } else {
-                    if (operation) // sum
-                        result += numbers[i];
-                    else          //product
-                        result *= numbers[i];
-                    i++;
-                }
-            }
-            if (result == target) {
-                sum+=target;
-                break;
-            }
+
+        if (compute_operation(numbers, target, result, number_of_operators, 0, 0)) {
+            sum+=target;
+            continue;
+        }
+        if (compute_operation(numbers, target, result, number_of_operators, 1, 0)) {
+            sum+=target;
+            continue;
         }
     }
 
     return std::to_string(sum);
-}
-
-size_t compute_number_of_digits(long int number) {
-    size_t digits = 1;
-    while(number/10) {
-        digits++;
-        number = number/10;
-    }
-    return digits;
 }
 
 AOC_DAY(Day07_2){
@@ -71,36 +95,17 @@ AOC_DAY(Day07_2){
         auto number_of_operators = numbers.size()-1;
         size_t number_of_permutations = std::pow(3,number_of_operators);
 
-        for (size_t permutation = 0; permutation<number_of_permutations; permutation++) {
-            size_t i=0;
-            for (int bit = number_of_operators-1; bit >= 0; --bit) {
-
-                int operation = (permutation / (int)std::pow(3,bit)) % 3;
-                if (bit == number_of_operators-1) //first computation
-                {
-                    if (operation == 0)      // sum
-                        result = numbers[i] + numbers[i+1];
-                    else if (operation ==  1) //product
-                        result = numbers[i]*numbers[i+1];
-                    else // concatenate
-                        result = numbers[i]*std::pow(10,compute_number_of_digits(numbers[i+1])) + numbers[i+1];
-                    i+=2;
-                } else {
-                    if (result > target)
-                        break;
-                    if (operation ==  0) // sum
-                        result += numbers[i];
-                    else if (operation ==  1) //product
-                        result *= numbers[i];
-                    else // concatenate
-                        result = result*std::pow(10,compute_number_of_digits(numbers[i])) + numbers[i];;
-                    i++;
-                }
-            }
-            if (result == target) {
-                sum+=target;
-                break;
-            }
+        if (compute_operation(numbers, target, result, number_of_operators, 0, 0, true)) {
+            sum+=target;
+            continue;
+        }
+        if (compute_operation(numbers, target, result, number_of_operators, 1, 0, true)) {
+            sum+=target;
+            continue;
+        }
+        if (compute_operation(numbers, target, result, number_of_operators, 2, 0, true)) {
+            sum+=target;
+            continue;
         }
     }
 
