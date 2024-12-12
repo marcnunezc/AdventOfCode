a1796dba43fb1834645ed2af50c92bf906099746
Marc Nuñez <marcnc94@gmail.com>
2024-12-12 20:41:37 +0100

    Add day11_2 using dfs and cache

--- 2024/Day11/day11.cpp
+++ 2024/Day11/day11.cpp
@@ -14,9 +14,7 @@ struct node {
 AOC_DAY(Day11_1) {
 
     std::string line;
-    std::size_t sum = 0;
 
-    std::vector<std::size_t> numbers;
     std::vector<node*> node_list;
     std::size_t node_count = 0;
     std::size_t x;
@@ -27,27 +25,17 @@ AOC_DAY(Day11_1) {
         if (!p_first) {
             p_first = p_node;
             p_node->value = x;
-            // cout <<"i "<< i << " "<< x << endl;
         } else {
-            // cout << "i " <<i<<" " << x << endl;
             p_node->value = x;
-            // cout << x << endl;
             node_list[i-1]->next = p_node;
         }
         node_list.push_back(p_node);
         node_count++;
         i++;
     }
-    // auto print_node = p_first;
-    // while (print_node) {
-    //     cout << print_node->value << " ";
-    //     print_node = print_node->next;
-    // }
-    // cout << endl;
 
     int max_rounds = 25;
     for (int count=0; count < max_rounds; count++) {
-        // cout << "count "<<count << " node_count " << node_count<<  endl;
         auto current_node = p_first;
         while (current_node) {
             auto number_of_digits = count_digits(current_node->value);
@@ -73,23 +61,55 @@ AOC_DAY(Day11_1) {
             }
             current_node=current_node->next;
         }
-        // auto print_node = p_first;
-        // while (print_node) {
-        //     cout << print_node->value << " ";
-        //     print_node = print_node->next;
-        // }
-        // cout << endl;
     }
 
     return std::to_string(node_count);
 }
 
 
+void advance_loop(std::size_t number, int current_depth, int final_depth, std::size_t& count, std::map<std::pair<std::size_t,int>, std::size_t>& cache) {
+    auto starting_count = count;
+    if (current_depth == final_depth) {
+        count++;
+        return;
+    }
+
+    if (cache.find({number,current_depth})!=cache.end()) {
+        count += cache[{number, current_depth}];
+        return;
+    }
+
+    auto number_of_digits = count_digits(number);
+    if (number == 0) {
+        advance_loop(1, current_depth+1, final_depth, count,cache);
+    } else if (number_of_digits % 2 == 1) {
+        advance_loop(number*2024, current_depth+1, final_depth, count,cache);
+    } else {
+        int upper = number / (int) std::pow(10,number_of_digits/2);
+        int lower = number % (int) std::pow(10,number_of_digits/2);
+        advance_loop(upper, current_depth+1, final_depth, count, cache);
+        advance_loop(lower, current_depth+1, final_depth, count, cache);
+    }
+    cache[{number, current_depth}] = count-starting_count;
+    return;
+}
+
 AOC_DAY(Day11_2){
 
-    std::string line;
-    std::size_t sum = 0;
 
+    std::vector<std::size_t> numbers;
+    std::map<std::pair<std::size_t,int>, std::size_t> cache;
+    std::size_t sum = 0;
+    std::size_t x;
+    while(cin >> x) {
+        numbers.push_back(x);
+    }
+    int max_rounds = 75;
+    for (auto number : numbers) {
+        std::size_t count=0;
+        advance_loop(number, 0, max_rounds, count, cache);
+        sum += count;
+    }
 
     return std::to_string(sum);
 }
