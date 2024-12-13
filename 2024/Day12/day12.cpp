c4d761d577ceb00a03723ff4be3db64cfb867595
Marc Nuñez <marcnc94@gmail.com>
2024-12-13 09:00:32 +0100

    Add day12 part 2

--- 2024/Day12/day12.cpp
+++ 2024/Day12/day12.cpp
@@ -14,7 +14,6 @@ void find_region(std::pair<int,int> current, std::set<std::pair<int, int>>& regi
         if (new_j<0 || new_j >= size_j)
             continue;
         if (matrix[new_i][new_j] == value && region_set.find({new_i,new_j}) == region_set.end()) {
-            // cout << "found " << (char) matrix[new_i][new_j] << " in " << new_i << " " << new_j << endl;
             region_set.insert({new_i,new_j});
             find_region({new_i,new_j}, region_set, matrix);
         }
@@ -48,11 +47,8 @@ AOC_DAY(Day12_1) {
     while(getline(cin,line)) {
         std::vector<int> row;
         for (auto ch : line) {
-            // cout << ch << " = " << (int) ch << endl;;
-            cout << ch;
             row.push_back((int) ch);
         }
-        cout << endl;
         matrix.push_back(row);
     }
 
@@ -64,7 +60,6 @@ AOC_DAY(Day12_1) {
     for (int i = 0; i<matrix.size(); i++) {
         for (int j = 0; j<matrix[0].size(); j++) {
             if (visited.find({i,j}) == visited.end()) {
-                // cout << "Region search start for " << (char) matrix[i][j] << " in " << i << " " << j << endl;
                 std::set<std::pair<int, int>> region_set;
                 region_set.insert({i,j});
                 find_region({i,j}, region_set, matrix);
@@ -73,35 +68,69 @@ AOC_DAY(Day12_1) {
                 regions_list.push_back(region_set);
                 auto area = region_set.size();
                 auto perimeter = compute_perimeter(region_set, matrix);
-                cout << "Region search finish " <<  (char) matrix[i][j] << "  size of region:" << region_set.size() << " perimetr "<<perimeter << endl;
                 sum += area*perimeter;
             }
         }
     }
 
-    cout << "Number of regions " <<  regions_list.size() << endl;
-
     return std::to_string(sum);
 }
 
 std::size_t compute_edges( std::set<std::pair<int, int>>& region_set, std::vector<std::vector<int>>& matrix) {
-    std::size_t perimeter = 0;
+    std::size_t n_edges = 0;
     auto size_i = matrix.size();
     auto size_j = matrix[0].size();
 
-    std::vector<std::pair<int,int>> neigh_deltas =  {{1,0},{-1,0},{0,1},{0,-1}};
-    for (auto& current : region_set) {
-        for (int i = 0; i<neigh_deltas.size(); i++) {
+    auto is_value = [&] (const std::pair<int,int> value) {
+        return region_set.find(value) != region_set.end();
+    };
 
-            auto new_i = neigh_deltas[i].first+current.first;
-            auto new_j = neigh_deltas[i].second+current.second;
-            if (new_i<0 || new_i >= size_i || new_j<0 || new_j >= size_j || matrix[new_i][new_j] != matrix[current.first][current.second]) {
-                perimeter++;
+    auto fill_edges = [&is_value] (bool& current_value_on, std::pair<int,int> pos, int i_ref, std::set<std::pair<int,bool>>& detected_edges) {
+        if (current_value_on && (!is_value(pos))) {
+            current_value_on = false;
+            detected_edges.insert({i_ref, current_value_on});
+        } else if(!current_value_on && is_value(pos) ) {
+            current_value_on = true;
+            detected_edges.insert({i_ref, current_value_on});
+        } 
+    };
+
+    auto add_non_repeated_edges= [](size_t& n_edges, std::set<std::pair<int,bool>>& detected_edges, std::set<std::pair<int,bool>>& previous_edges) {
+        for (auto edge : detected_edges) {
+            if (previous_edges.find(edge) == previous_edges.end()) {
+                n_edges++;
             }
         }
+    };
+
+
+    std::set<std::pair<int,bool>> previous_edges;
+    for (int i = 0; i<matrix.size()+1; i++) {
+        bool current_value_on = false;
+        std::set<std::pair<int,bool>> detected_edges;
+        for (int j = 0; j<matrix[0].size()+1; j++) {
+            fill_edges(current_value_on, {i,j}, j, detected_edges);
+        }
+        add_non_repeated_edges(n_edges, detected_edges, previous_edges);
+        previous_edges = detected_edges;
     }
-    return perimeter;
+    previous_edges.clear();
+    
+    for (int j = 0; j<matrix[0].size()+1; j++) {
+        bool current_value_on = false;
+        std::set<std::pair<int,bool>> detected_edges;
+        for (int i = 0; i<matrix.size()+1; i++) {
+
+            fill_edges(current_value_on, {i,j}, i , detected_edges);
+
+        }
+        add_non_repeated_edges(n_edges, detected_edges, previous_edges);
+        previous_edges = detected_edges;
+    }
+    return n_edges;
 }
+
+
 AOC_DAY(Day12_2){
     std::string line;
     std::size_t sum = 0;
@@ -110,11 +139,8 @@ AOC_DAY(Day12_2){
     while(getline(cin,line)) {
         std::vector<int> row;
         for (auto ch : line) {
-            // cout << ch << " = " << (int) ch << endl;;
-            cout << ch;
             row.push_back((int) ch);
         }
-        cout << endl;
         matrix.push_back(row);
     }
 
@@ -126,7 +152,6 @@ AOC_DAY(Day12_2){
     for (int i = 0; i<matrix.size(); i++) {
         for (int j = 0; j<matrix[0].size(); j++) {
             if (visited.find({i,j}) == visited.end()) {
-                // cout << "Region search start for " << (char) matrix[i][j] << " in " << i << " " << j << endl;
                 std::set<std::pair<int, int>> region_set;
                 region_set.insert({i,j});
                 find_region({i,j}, region_set, matrix);
@@ -135,13 +160,10 @@ AOC_DAY(Day12_2){
                 regions_list.push_back(region_set);
                 auto area = region_set.size();
                 auto perimeter = compute_edges(region_set, matrix);
-                cout << "Region search finish " <<  (char) matrix[i][j] << "  size of region:" << region_set.size() << " perimetr "<<perimeter << endl;
                 sum += area*perimeter;
             }
         }
     }
-
-    cout << "Number of regions " <<  regions_list.size() << endl;
 
     return std::to_string(sum);
 }
