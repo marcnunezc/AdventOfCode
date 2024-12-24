8b158fe6030bd8c85d96a4a7ad8627fcbc96ef1a
Marc Nuñez <marcnc94@gmail.com>
2024-12-24 11:54:46 +0100

    Add day 24 part 1

--- 2024/Day24/day24.cpp
+++ 2024/Day24/day24.cpp
@@ -3,6 +3,7 @@ AOC_DAY(Day24_1) {
     std::size_t sum = 0;
 
     std::map<std::string, bool> gates;
+    std::set<std::string> registered_gates;
     std::vector<std::tuple<std::string, std::string, std::string, std::string>> operands_list;
 
 
@@ -14,7 +15,7 @@ AOC_DAY(Day24_1) {
         std::string gate = line.substr(0,line.find(':'));
         bool value = (bool) std::stoi(line.substr(line.find(':')+2));
         gates[gate] = value;
-        cout << "Gate: " << gate << " Value: " << value << endl;
+        registered_gates.insert(gate);
     }
 
     while(getline(cin,line)) {
@@ -25,24 +26,28 @@ AOC_DAY(Day24_1) {
         if (result.find('z') != std::string::npos) {
             z_set.insert(result);
         }
-
+        registered_gates.insert(result);
         operands_list.push_back(std::make_tuple(oper1, bool_operator, oper2, result));
     }
-    for (size_t i = 0; i < operands_list.size(); i++) {
-        auto [oper1, bool_operator, oper2, result] = operands_list[i];
-        cout << oper1 << " " <<bool_operator << " "<<oper2 << " " << result << endl;
-        cout << gates[oper1] << " " << gates[oper2] << endl;
-        if (bool_operator == "AND") {
-            gates[result] = gates[oper1] & gates[oper2];
-        } else if (bool_operator == "OR") {
-            gates[result] = gates[oper1] | gates[oper2];
-        } else if (bool_operator == "XOR") {
-            gates[result] = gates[oper1] != gates[oper2];
+
+    while (registered_gates.size() != gates.size()) {
+        for (size_t i = 0; i < operands_list.size(); i++) {
+            auto [oper1, bool_operator, oper2, result] = operands_list[i];
+            if (gates.find(oper1) == gates.end() || gates.find(oper2) == gates.end() || gates.find(result) != gates.end()) {
+                continue;
+            }
+            if (bool_operator == "AND") {
+                gates[result] = gates[oper1] & gates[oper2];
+            } else if (bool_operator == "OR") {
+                gates[result] = gates[oper1] | gates[oper2];
+            } else if (bool_operator == "XOR") {
+                gates[result] = gates[oper1] != gates[oper2];
+            }
         }
-        cout << "Result: " << gates[result] << endl;
     }
+    size_t bit_pos = 0;
     for (auto z : z_set) {
-        cout << z << " " << gates[z] << endl;
+        sum +=  (size_t) gates[z] << std::stoi(z.substr(1));
     }
 
     return std::to_string(sum);
